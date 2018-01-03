62126f8d6c437fdbf32d38718b8b839e9d044639
zenghangyu <857240667@qq.com>
2018-01-03 17:32:53 +0800

    购物车

--- README.md
+++ README.md
@@ -8,3 +8,21 @@ The Learning process of Vue
 > 2018.1.1 制作一个简单的tab切换 实践了v-show的应用，了解v-if与v-show的区别
 
 > 2018.1.2 正在制作一个简单的购物车，期间用到了vue-resource以及vue的过滤器 filters 在制作期间遇到了vue-resource的跨域问题，然后花时间用node配置一个本地的服务器。搭建成功之后发现引入的模块只能解析html。真是悲催〒▽〒，明天继续努力。
+
+>2018.1.3 在使用过滤器filters的过程中深度学习了 filters的使用场景:
+
+
+
+>#### 其可以在（**双花括号插值和 v-bind 表达式** ）中使用  使用方法为
+>
+>```
+><!-- 在双花括号中 -->
+>{{ message | capitalize }}
+>
+><!-- 在 `v-bind` 中 -->
+><div v-bind:id="rawId | formatId"></div>
+>```
+
+> 组件内定义为filters:{}  全局定义为Vue.filter('',function(){})
+>
+> 过滤器函数总接收表达式的值 (之前的操作链的结果) 作为第一个参数。
--- vue2.0-ShoppingCart/js/cart.js
+++ vue2.0-ShoppingCart/js/cart.js
@@ -1,19 +1,82 @@
 var vm = new Vue({
     el: "#app",
     data: {
-        cart: ''
+        cart: '',
+        cartList: '',
+        checkFlag: false,
+        totalPrice: 0
     },
-    filters: {
 
+    /*
+        过滤器函数总接收表达式的值 (之前的操作链的结果) 作为第一个参数。在上述例子中，capitalize 过滤器函数将会收到 message 的值作为第一个参数。
+    **/
+    filters: {
+        formMoney: function(value) {
+            return '$  ' + value.toFixed(2);
+        }
     },
     mounted: function() {
-        this.cartView();
+        this.$nextTick(function() {
+            vm.cartView();
+        });
     },
     methods: {
         cartView: function() {
+            var _this = this;
             this.$http.get('./data/cartData.json', { 'id': 123 }).then(function(res) {
+                console.log(res.data.result);
+                _this.cartList = res.data.result.list
+            })
+        },
+        changeprice: function(value, way) {
+            if (way > 0) {
+                value.productQuantity++;
+            } else {
+                value.productQuantity <= 2 ? value.productQuantity = 1 : value.productQuantity--;
 
+            }
+        },
+        selectProduct: function(item) {
+            if (typeof item.checked == 'undefined') {
+                this.$set(item, 'checked', true);
+            } else {
+                item.checked = !item.checked
+            }
+            this.calcTotalPrice();
+        },
+        checkAll: function(flag) {
+            if (flag) {
+                this.checkFlag = !this.checkFlag;
+                var _this = this;
+                this.cartList.forEach(function(item, index) {
+                    if (typeof item.checked == 'undefined') {
+                        _this.$set(item, 'checked', _this.checkFlag);
+                    } else {
+                        item.checked = _this.checkFlag;
+                    }
+                })
+            } else {
+                var _this = this;
+                this.cartList.forEach(function(item, index) {
+                    _this.$set(item, 'checked', false);
+                    _this.checkFlag = false;
+
+                })
+            }
+            this.calcTotalPrice();
+
+        },
+        calcTotalPrice: function() {
+            var _this = this;
+            this.totalPrice = 0;
+            this.cartList.forEach(function(item, index) {
+                if (item.checked) {
+                    _this.totalPrice += item.productPrice * item.productQuantity;
+                }
             })
         }
     }
+})
+Vue.filter('money', function(value, type) {
+    return '$  ' + value.toFixed(2) + type;
 })
